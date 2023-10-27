a4f18ac5328208068584cb802584c7f3ced2a530
SamuelVargas2674489 <148775612+SamuelVargas2674489@users.noreply.github.com>
2023-10-27 09:10:46 -0500

    semaforo2ULTIMO.ino

--- semaforo2ULTIMO.ino
+++ semaforo2ULTIMO.ino
@@ -47,7 +47,7 @@ void loop()
   if (analogRead(POTEN)>300)
   {
   digitalWrite (7 , HIGH);
-  delay(1500);
+  delay(5000);
   digitalWrite (6,HIGH);
   delay(1000);
   digitalWrite (7 , LOW);
@@ -55,7 +55,7 @@ void loop()
   digitalWrite (6 , LOW);
   delay(100);
   digitalWrite (5, HIGH);
-  delay(1500);
+  delay(3000);
   digitalWrite (5, LOW);
   delay(100);
   digitalWrite (6, HIGH);
