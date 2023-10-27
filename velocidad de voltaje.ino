11f952ad06482b8c48cccbb40b5a4659b12eeb53
SamuelVargas2674489 <148775612+SamuelVargas2674489@users.noreply.github.com>
2023-10-27 09:04:07 -0500

    velocidad de voltaje.ino

--- velocidad de voltaje.ino
+++ velocidad de voltaje.ino
@@ -1,10 +1,14 @@
 void setup() {
+  
   Serial.begin(9600);
 }
 
 void loop() {
-  int valor = analogRead(A0); 
-  Serial.print("Valor en A0: ");
-  Serial.println(valor);
-  delay(1000); 
+ 
+  int sensorValue = analogRead(A0);
+  float voltage = (sensorValue / 1023.0) * 5.0;
+  Serial.print("Voltaje: ");
+  Serial.print(voltage, 2); 
+  Serial.println(" V");
+  delay(100); 
 }
